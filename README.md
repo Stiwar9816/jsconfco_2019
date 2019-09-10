19215d33697836f736c1addbdb01118a535d933f
Milton-code <milton.website@gmail.com>
2019-09-10 17:08:36 -0500

    Se agregaron los archivos de entorno de desarrollo

---
+++ README.md
@@ -0,0 +1,6 @@
+# JSConfco 2019 Colombia
+
+Sitio web en construcción
+
+
+#### Entorno de desarrollo con  `Gatsby` ;)
---
+++ src/components/layout.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
+
+// import Header from './header'
+// import Footer from './footer'
+// import SEO from './seo'
+// import Tuned from '../components/tuned'
+
+// import './layout.scss'
+
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={() => (
+      <>
+        {/* <SEO title="NodeConfCo" keywords={[`nodeconfco`, `colombia`]} /> */}
+        {/* <Header /> */}
+        <div>
+          {children}
+          </div>
+        {/* <Tuned /> */}
+        {/* <Footer /> */}
+      </>
+    )}
+  />
+)
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default Layout
